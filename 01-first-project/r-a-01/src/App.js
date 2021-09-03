fda1a0110c30c35f78ce87a3f6070ce8d3533f41
IDPbIH <andrey.korneyev@gmail.com>
2021-09-03 10:15:57 +0300

    11a

--- 01-first-project/r-a-01/src/App.js
+++ 01-first-project/r-a-01/src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologies';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='/logo.gif'></img>
+      </header>
+      <nav className='nav'>
+        <div>Profile</div>
+        <div>Messages </div>
+      </nav>
+      <div className='content'>Main content</div>
     </div>
   );
 }
