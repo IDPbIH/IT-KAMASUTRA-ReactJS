0bda4d2facd5f399365c81f4c8a579ac87a734ca
IDPbIH <andrey.korneyev@gmail.com>
2021-09-03 15:11:07 +0300

    13

--- 01-first-project/r-a-01/src/App.js
+++ 01-first-project/r-a-01/src/App.js
@@ -1,17 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 const App = () => {
   return (
     <div className='app-wrapper'>
-      <header className='header'>
-        <img src='/logo.gif'></img>
-      </header>
-      <nav className='nav'>
-        <div>Profile</div>
-        <div>Messages </div>
-      </nav>
-      <div className='content'>Main content</div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ 01-first-project/r-a-01/src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://i.pinimg.com/564x/e9/2d/47/e92d4726f77c744f60f0bd532cfbda89.jpg'></img>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
