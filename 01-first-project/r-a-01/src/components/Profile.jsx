61212d94b133d1525d395609820980ace31ccfa8
Andrey <andrey.korneyev@gmail.com>
2021-09-06 22:17:32 +0300

    14b

--- 01-first-project/r-a-01/src/components/Profile.jsx
+++ 01-first-project/r-a-01/src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={s.content}>
             <div>
                 <img src='https://i.pinimg.com/564x/e9/2d/47/e92d4726f77c744f60f0bd532cfbda89.jpg'></img>
             </div>
@@ -14,11 +15,11 @@ const Profile = () => {
                 <div>
                     New post
                 </div>
-                <div>
-                    <div>
+                <div className={s.posts}>
+                    <div className={s.item}>
                         post 1
                     </div>
-                    <div>
+                    <div className={s.item}>
                         post 2
                     </div>
                 </div>
