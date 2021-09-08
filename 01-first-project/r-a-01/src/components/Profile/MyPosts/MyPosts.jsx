15e8a85b191b79188017b44d52bb149750dcbe63
IDPbIH <andrey.korneyev@gmail.com>
2021-09-08 15:38:53 +0300

    16

--- 01-first-project/r-a-01/src/components/Profile/MyPosts/MyPosts.jsx
+++ 01-first-project/r-a-01/src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,6 +11,8 @@ const MyPosts = () => {
                 <button>Add post</button>
             </div>
             <div className={s.posts}>
+                <Post message='Hi, how are you?' like='2'/>
+                <Post message="It's my first post" like='31'/>
                 <Post />
                 <Post />
                 <Post />
--- 01-first-project/r-a-01/src/components/Profile/MyPosts/Post/Post.jsx
+++ 01-first-project/r-a-01/src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={s.item}>
             <img src='https://cdn.jpg.wtf/futurico/fb/cd/1496907487-fbcdfc3e065d421ae65c479908fbfc92.png'></img>
+            {props.message}
             post 1
             <div>
+                {props.like}
                 <span>Like</span>
             </div>
         </div>
