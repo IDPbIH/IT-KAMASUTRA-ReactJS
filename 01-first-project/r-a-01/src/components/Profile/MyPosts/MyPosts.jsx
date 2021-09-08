6bd2640c6497fd9ce179505b7d6c65dd7bb451b0
IDPbIH <andrey.korneyev@gmail.com>
2021-09-08 15:36:05 +0300

    Revert "15"
    This reverts commit f7e6ba65c5e080ae83e7948917e2cd90a8d24dcc.

--- 01-first-project/r-a-01/src/components/Profile/MyPosts/MyPosts.jsx
+++ 01-first-project/r-a-01/src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,8 +11,9 @@ const MyPosts = () => {
                 <button>Add post</button>
             </div>
             <div className={s.posts}>
-                <Post message='Hi, how are you?' like='2'/>
-                <Post message="It's my first post" like='31'/>
+                <Post />
+                <Post />
+                <Post />
             </div>
         </div>
 
--- 01-first-project/r-a-01/src/components/Profile/MyPosts/Post/Post.jsx
+++ 01-first-project/r-a-01/src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = (props) => {
+const Post = () => {
     return (
         <div className={s.item}>
             <img src='https://cdn.jpg.wtf/futurico/fb/cd/1496907487-fbcdfc3e065d421ae65c479908fbfc92.png'></img>
-            {props.message}
+            post 1
             <div>
-                {props.like} 
                 <span>Like</span>
             </div>
         </div>
