f7e6ba65c5e080ae83e7948917e2cd90a8d24dcc
IDPbIH <andrey.korneyev@gmail.com>
2021-09-08 15:35:10 +0300

    15

--- 01-first-project/r-a-01/src/components/Profile/MyPosts/MyPosts.jsx
+++ 01-first-project/r-a-01/src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,9 +11,8 @@ const MyPosts = () => {
                 <button>Add post</button>
             </div>
             <div className={s.posts}>
-                <Post />
-                <Post />
-                <Post />
+                <Post message='Hi, how are you?' like='2'/>
+                <Post message="It's my first post" like='31'/>
             </div>
         </div>
 
