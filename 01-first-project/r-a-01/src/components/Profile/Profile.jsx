90914e39ee0b6f53a6e3321133de47c42d58aaa4
Andrey <andrey.korneyev@gmail.com>
2021-09-10 21:08:38 +0300

    18b

--- 01-first-project/r-a-01/src/components/Profile/Profile.jsx
+++ 01-first-project/r-a-01/src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src='https://i.pinimg.com/564x/e9/2d/47/e92d4726f77c744f60f0bd532cfbda89.jpg'></img>
             </div>
