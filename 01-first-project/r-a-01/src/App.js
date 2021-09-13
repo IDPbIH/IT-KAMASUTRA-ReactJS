24a4e259d9e8ca03209676779bd1a4c857bc0794
Andrey <andrey.korneyev@gmail.com>
2021-09-13 22:08:36 +0300

    22

--- 01-first-project/r-a-01/src/App.js
+++ 01-first-project/r-a-01/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, NavLink, Route} from "react-router-dom";
 import Dialogs from "./components/Dialogs/Dialogs";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
+import React from "react";
 
 const App = (props) => {
     return (
