c7d548b49af8a5b986ad576fbfac0f20a88c9623
Andrey <andrey.korneyev@gmail.com>
2021-09-14 00:05:15 +0300

    23

--- 01-first-project/r-a-01/src/components/Profile/MyPosts/MyPosts.jsx
+++ 01-first-project/r-a-01/src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,18 +4,22 @@ import Post from './Post/Post';
 
 const MyPosts = () => {
     return (
-        <div>
-            My posts
+        <div className={s.postsBlock}>
+            <h3>My posts</h3>
             <div>
-                <textarea></textarea>
-                <button>Add post</button>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <button>Add post</button>
+                </div>
             </div>
             <div className={s.posts}>
-                <Post message='Hi, how are you?' likesCount='2' />
-                <Post message="It's my first post" likesCount='31' />
-                <Post />
-                <Post />
-                <Post />
+                <Post message='Hi, how are you?' likesCount='2'/>
+                <Post message="It's my first post" likesCount='31'/>
+                <Post/>
+                <Post/>
+                <Post/>
             </div>
         </div>
 
--- 01-first-project/r-a-01/src/components/Profile/Profile.jsx
+++ 01-first-project/r-a-01/src/components/Profile/Profile.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
     return (
         <div>
-            <div>
-                <img src='https://i.pinimg.com/564x/e9/2d/47/e92d4726f77c744f60f0bd532cfbda89.jpg'></img>
-            </div>
-            <div>
-                ava + description
-            </div>
-            <MyPosts />
+            <ProfileInfo/>
+            <MyPosts/>
         </div>
     )
 }
