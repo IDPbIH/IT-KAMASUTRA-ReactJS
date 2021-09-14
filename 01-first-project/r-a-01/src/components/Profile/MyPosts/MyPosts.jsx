81399c0fbb5595f98be876fa84fd5522f98c2c80
Andrey <andrey.korneyev@gmail.com>
2021-09-14 23:40:10 +0300

    25

--- 01-first-project/r-a-01/src/components/Profile/MyPosts/MyPosts.jsx
+++ 01-first-project/r-a-01/src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,10 +4,12 @@ import Post from './Post/Post';
 
 const MyPosts = () => {
 
-    let postsData = [
+    let posts = [
         {id: 1, message: 'Hi, how are you?', likesCount:2},
         {id: 2, message: 'It\'s my first post', likesCount:31},
     ];
+
+    let postElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
 
     return (
         <div className={s.postsBlock}>
@@ -21,8 +23,7 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message={postsData[0].message} likesCount={postsData[0].likesCount}/>
-                <Post message={postsData[1].message} likesCount={postsData[1].likesCount}/>
+                {postElements}
             </div>
         </div>
 
