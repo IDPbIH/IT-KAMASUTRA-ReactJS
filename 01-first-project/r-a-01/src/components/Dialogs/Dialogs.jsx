3c7d92fb99509f91f764cc918f5d69a32b808a90
Andrey <andrey.korneyev@gmail.com>
2021-09-23 14:21:14 +0300

    43

--- 01-first-project/r-a-01/src/components/Dialogs/Dialogs.jsx
+++ 01-first-project/r-a-01/src/components/Dialogs/Dialogs.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
-import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/dialogs-reducer";
 
 const Dialogs = (props) => {
+    let state = props.dialogsPage;
 
-    let state = props.store.getState().dialogsPage;
     let dialogsElements = state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+
     let messagesElements =
         state.messages.map(m => {
             if (m.id % 2) {
@@ -16,15 +16,16 @@ const Dialogs = (props) => {
                 return <div className={s.messageRight}><Message message={m.message}/></div>;
             }
         });
+
     let newMessageBody = state.newMessageBody;
 
     let onSendMessageClick = () => {
-        props.store.dispatch(sendMessageCreator());
+        props.sendMessage();
     };
 
     let onNewMessageChange = (e) => {
-        let body =  e.target.value;
-        props.store.dispatch(updateNewMessageBodyCreator(body));
+        let body = e.target.value;
+        props.updateNewMessageBody(body);
     }
 
     return (
--- 01-first-project/r-a-01/src/components/Profile/MyPosts/MyPosts.jsx
+++ 01-first-project/r-a-01/src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
-import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
 
 const MyPosts = (props) => {
 
@@ -9,13 +8,13 @@ const MyPosts = (props) => {
 
     let newPostElement = React.createRef();
 
-    let addPost = () => {
-        props.dispatch(addPostActionCreator());
+    let onAddPost = () => {
+        props.addPost();
     };
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        props.dispatch(updateNewPostTextActionCreator(text));
+        props.updateNewPostText(text);
     }
 
     return (
@@ -30,14 +29,13 @@ const MyPosts = (props) => {
                     />
                 </div>
                 <div>
-                    <button onClick={addPost}>Add post</button>
+                    <button onClick={onAddPost}>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
                 {postElements}
             </div>
         </div>
-
     )
 }
 
