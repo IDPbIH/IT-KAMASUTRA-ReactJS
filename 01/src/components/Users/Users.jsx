0766a1e622a1c5840084d4a0a297efd788e3eef1
IDPbIH <andrey.korneyev@gmail.com>
2021-10-13 17:30:03 +0300

    52

--- 01/src/components/Users/Users.jsx
+++ 01/src/components/Users/Users.jsx
@@ -4,45 +4,18 @@ import * as axios from 'axios';
 import userPhoto from '../../assets/images/user.png';
 
 const Users = (props) => {
-
-    if (props.users.length === 0) {
-
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items);
-        });
-
-        // props.setUsers(
-        //     [
-        //         {
-        //             id: 1,
-        //             photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg',
-        //             followed: false,
-        //             fullName: 'Dmitry',
-        //             status: 'I am a boss',
-        //             location: {city: 'Minsk', country: 'Belarus'}
-        //         },
-        //         {
-        //             id: 2,
-        //             photoUrl: 'https://www.eg.ru/wp-content/uploads/2021/03/sasha-grey101910.jpg',
-        //             followed: true,
-        //             fullName: 'Sasha',
-        //             status: 'I am a boss too',
-        //             location: {city: 'Moscow', country: 'Russia'}
-        //         },
-        //         {
-        //             id: 3,
-        //             photoUrl: 'https://resizer.mail.ru/p/93669016-d551-591b-867d-e14c46852ecc/AAACpN7QLvUV-jjoAwzTMN83nJQS6V7sCx20jIys2AsqiNhqrNZlDl5kP02qzGlJiAzoVVB1czjtW-g_bxHr-RldA9I.jpg',
-        //             followed: false,
-        //             fullName: 'Andrew',
-        //             status: 'I am a boss too',
-        //             location: {city: 'Kiev', country: 'Ukraine'}
-        //         }
-        //     ]
-        // )
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items);
+            });
+        }
     }
+
 
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {
                 props.users.map(u => <div key={u.id}>
                     <span>
