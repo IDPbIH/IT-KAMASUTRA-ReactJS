944b0c18778d44a667edc48692509eb2de49409d
IDPbIH <andrey.korneyev@gmail.com>
2021-10-13 17:15:25 +0300

    51-2

--- 01/src/components/Users/Users.jsx
+++ 01/src/components/Users/Users.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './Users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.png';
 
 const Users = (props) => {
 
     if (props.users.length === 0) {
-        
+
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers()
+            props.setUsers(response.data.items);
         });
 
         // props.setUsers(
@@ -45,7 +47,7 @@ const Users = (props) => {
                 props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img className={styles.userPhoto} src={u.photoUrl}/>
+                            <img className={styles.userPhoto} src={u.photos.small != null ? u.photos : userPhoto} />
                         </div>
                         <div>
                             {u.followed ?
@@ -57,11 +59,11 @@ const Users = (props) => {
                     </span>
                     <span>
                         <span>
-                            <div>{u.fullName}</div><div>{u.status}</div>
+                            <div>{u.name}</div><div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
                         </span>
                     </span>
                 </div>)
