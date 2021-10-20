acb056373a4ad2154b18330f2806d28c82a28038
IDPbIH <andrey.korneyev@gmail.com>
2021-10-20 17:37:31 +0300

    59

--- 01/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ 01/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import Preloader from '../../common/Preloader/Preloader';
 import s from './ProfileInfo.module.css';
 
-const ProfileInfo = () => {
-    return (
-        <div>
+const ProfileInfo = (props) => {
+    if (!props.profile) {
+        return <Preloader />
+    } else {
+        return (
             <div>
-                <img src='https://i.pinimg.com/564x/e9/2d/47/e92d4726f77c744f60f0bd532cfbda89.jpg'></img>
+                {/* <div>
+                    <img src='https://i.pinimg.com/564x/e9/2d/47/e92d4726f77c744f60f0bd532cfbda89.jpg'></img>
+                </div> */}
+                <div className={s.descriptionBlock}>
+                    <div>id = {props.profile.userId}</div>
+                    <div><img src={props.profile.photos.large} /></div>
+                    <div>{props.profile.fullName}</div>
+                    <div>{props.profile.aboutMe}</div>
+                    <div>vk: {props.profile.contacts.vk}</div>
+                </div>
             </div>
-            <div className={s.descriptionBlock}>
-                ava + description
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default ProfileInfo;
--- 01/src/components/Users/Users.jsx
+++ 01/src/components/Users/Users.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from './Users.module.css';
 import userPhoto from '../../assets/images/user.png';
+import { NavLink } from "react-router-dom";
 
 let Users = (props) => {
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
@@ -10,19 +11,20 @@ let Users = (props) => {
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
-    
+
     return <div>
         <div>
             {pages.map(p => {
                 return <span className={props.currentPage === p && styles.selectedPage} onClick={(e) => { props.onPageChanged(p); }}> {p} </span>
             })}
         </div>
-        
         {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img className={styles.userPhoto} src={u.photos.small != null ? u.photos.small : userPhoto} />
+                        <NavLink to={'/profile/' + u.id}>
+                            <img className={styles.userPhoto} src={u.photos.small != null ? u.photos.small : userPhoto} />
+                        </NavLink>
                     </div>
                     <div>
                         {u.followed ?
