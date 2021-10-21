d21da1295629c3b9ad66e2d4b9d0ed5b3667c733
IDPbIH <andrey.korneyev@gmail.com>
2021-10-21 16:16:28 +0300

    61

--- 01/src/App.js
+++ 01/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import './App.css';
 import {BrowserRouter, Route} from "react-router-dom";
-import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
@@ -9,6 +8,7 @@ import UsersContainer from "./components/Users/UsersContainer";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 
 
@@ -16,7 +16,7 @@ const App = () => {
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
-                <Header/>
+                <HeaderContainer/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
                     <Route path='/dialogs' render={() => <DialogsContainer/>}/>
