914974f9959845e89b608dc2eb242f5f05b5ba60
Andrey <andrey.korneyev@gmail.com>
2021-10-24 21:24:17 +0300

    63

---
+++ 01/src/api/api.js
@@ -0,0 +1,20 @@
+import * as axios from 'axios';
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: { "API-KEY": "22b94168-25c1-42a1-b082-01d927be30be" }
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`,
+            {
+                withCredentials: true
+            })
+            .then(response => {
+                return response.data;
+            });
+    }
+
+}
