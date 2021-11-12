8681d029c15454c9d77195b9c568167af36f589d
Andrey <andrey.korneyev@gmail.com>
2021-11-12 22:32:21 +0300

    89

--- react-way-of-samurai/src/redux/profile-reducer.test.js
+++ react-way-of-samurai/src/redux/profile-reducer.test.js
@@ -1,18 +1,46 @@
-import profileReducer, { addPostActionCreator } from "./profile-reducer";
+import profileReducer, { addPostActionCreator, deletePost } from "./profile-reducer";
 
+let state = {
+    posts: [
+        { id: 1, message: 'Hi, how are you?', likesCount: 2 },
+        { id: 2, message: 'It\'s my first post', likesCount: 31 },
+        { id: 3, message: 'Yo', likesCount: 2 },
+        { id: 4, message: 'Yo', likesCount: 3 }
+    ]
+}
 test('length of post should be incremented', () => {
     // 1. TestData
     let action = addPostActionCreator('dpbih.com');
-    let state = {
-        posts: [
-            { id: 1, message: 'Hi, how are you?', likesCount: 2 },
-            { id: 2, message: 'It\'s my first post', likesCount: 31 },
-            { id: 3, message: 'Yo', likesCount: 2 },
-            { id: 4, message: 'Yo', likesCount: 3 }
-        ]
-    }
     // 2. Action
     let newState = profileReducer(state,action);
     // 3. Expectation
     expect(newState.posts.length).toBe(5);
+});
+test('message of new post should be "dpbih.com"', () => {
+    // 1. TestData
+    let action = addPostActionCreator('dpbih.com');
+    
+    // 2. Action
+    let newState = profileReducer(state,action);
+    // 3. Expectation
+    expect(newState.posts[4].message).toBe('dpbih.com');
+});
+test('after deliting length of messages should be decrement', () => {
+    // 1. TestData
+    let action = deletePost(1);
+    
+    // 2. Action
+    let newState = profileReducer(state,action);
+    // 3. Expectation
+    expect(newState.posts.length).toBe(3);
+});
+
+test('after deliting length shouldn`t be decrement if id is incorrect', () => {
+    // 1. TestData
+    let action = deletePost(1000);
+    
+    // 2. Action
+    let newState = profileReducer(state,action);
+    // 3. Expectation
+    expect(newState.posts.length).toBe(4);
 });
