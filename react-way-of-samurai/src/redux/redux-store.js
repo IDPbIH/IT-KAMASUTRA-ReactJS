61fdfff4baf68f529aecee2b7fd3c40b68807f02
IDPbIH <andrey.korneyev@gmail.com>
2021-11-16 14:10:34 +0300

    91

--- react-way-of-samurai/src/redux/redux-store.js
+++ react-way-of-samurai/src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import profileReducer from './profile-reducer';
 import dialogsReducer from './dialogs-reducer';
 import sidebarReducer from './sidebar-reducer';
@@ -19,8 +19,12 @@ let reducers = combineReducers({
 }
 );
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+//для расширения Redux DevTools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
-window.store = store;
+// let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+
+window.___store___ = store;
 
 export default store;
