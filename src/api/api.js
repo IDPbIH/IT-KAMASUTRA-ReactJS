273d8a4cab5eb1b304d72feff146d47ced4f4eca
IDPbIH <andrey.korneyev@gmail.com>
2021-11-19 15:43:25 +0300

    98

--- src/api/api.js
+++ src/api/api.js
@@ -8,18 +8,19 @@ const instance = axios.create({
 
 export const usersAPI = {
     getUsers(currentPage = 1, pageSize = 10) {
-        return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => {
-            return response.data;
-        });
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data;
+            });
     },
-    follow(userid) {
-        return instance.post(`follow/${userid}`);
+    follow(userId) {
+        return instance.post(`follow/${userId}`)
     },
     unfollow(userId) {
-        return instance.delete(`follow/${userId}`);
+        return instance.delete(`follow/${userId}`)
     },
     getProfile(userId) {
-        console.warn('Obsolete method. Please use profileAPI object.')
+        console.warn('Obsolete method. Please profileAPI object.')
         return profileAPI.getProfile(userId);
     }
 }
@@ -35,14 +36,17 @@ export const profileAPI = {
         return instance.put(`profile/status`, { status: status });
     },
     savePhoto(photoFile) {
-        var formData = new FormData();
-        formData.append('image', photoFile);
+        const formData = new FormData();
+        formData.append("image", photoFile);
+
         return instance.put(`profile/photo`, formData, {
-            'Content-Type': 'multipart/form-data'
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
         });
     },
     saveProfile(profile) {
-        return instance.put(`profile`, profile );
+        return instance.put(`profile`, profile);
     }
 }
 
@@ -50,10 +54,16 @@ export const authAPI = {
     me() {
         return instance.get(`auth/me`);
     },
-    login(email, password, remebmerMe = false) {
-        return instance.post(`auth/login`, { email, password, remebmerMe });
+    login(email, password, rememberMe = false, captcha = null) {
+        return instance.post(`auth/login`, { email, password, rememberMe, captcha });
     },
     logout() {
         return instance.delete(`auth/login`);
-    },
+    }
+}
+
+export const securityAPI = {
+    getCaptchaUrl() {
+        return instance.get(`security/get-captcha-url`);
+    }
 }
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,21 +1,26 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
 import { reduxForm } from "redux-form";
-import { required } from "../../utils/validators/validators";
 import { createField, Input } from "../common/FormsControls/FormsControls";
-import { login } from '../../redux/auth-reducer';
-import { Redirect } from 'react-router-dom';
-import s from '../../components/common/FormsControls/FormsControls.module.css';
+import { required } from "../../utils/validators/validators";
+import { connect } from "react-redux";
+import { login } from "../../redux/auth-reducer";
+import { Redirect } from "react-router-dom";
+import style from "./../common/FormsControls/FormsControls.module.css"
 
-const LoginForm = ({ handleSubmit, error }) => {
+const LoginForm = ({ handleSubmit, error, captchaUrl }) => {
     return (
         <form onSubmit={handleSubmit}>
-            {createField('Email', 'email', [required], Input)}
-            {createField('Password', 'password', [required], Input, { type: 'password' })}
-            {createField(null, 'rememberMe', [], Input, { type: 'checkbox' }, 'remember me')}
-            {error && <div className={s.formSummaryError}>
+            {createField("Email", "email", [required], Input)}
+            {createField("Password", "password", [required], Input, { type: "password" })}
+            {createField(null, "rememberMe", [], Input, { type: "checkbox" }, "remember me")}
+
+            {captchaUrl && <img src={captchaUrl} />}
+            {captchaUrl && createField("Symbols from image", "captcha", [required], Input, {})}
+
+            {error && <div className={style.formSummaryError}>
                 {error}
-            </div>}
+            </div>
+            }
             <div>
                 <button>Login</button>
             </div>
@@ -27,20 +32,20 @@ const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm)
 
 const Login = (props) => {
     const onSubmit = (formData) => {
-        props.login(formData.email, formData.password, formData.rememberMe)
+        props.login(formData.email, formData.password, formData.rememberMe, formData.captcha);
     }
 
     if (props.isAuth) {
-        return <Redirect to={'/profile'} />
+        return <Redirect to={"/profile"} />
     }
 
     return <div>
         <h1>Login</h1>
-        <LoginReduxForm onSubmit={onSubmit} />
+        <LoginReduxForm onSubmit={onSubmit} captchaUrl={props.captchaUrl} />
     </div>
 }
-
 const mapStateToProps = (state) => ({
+    captchaUrl: state.auth.captchaUrl,
     isAuth: state.auth.isAuth
 })
 
