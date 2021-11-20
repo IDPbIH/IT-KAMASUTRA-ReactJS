bf3faf829e8069e6fd503d34fbd0f80e64e28800
Andrey <andrey.korneyev@gmail.com>
2021-11-20 13:01:00 +0300

    99-1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import './App.css';
-import { HashRouter, BrowserRouter, Route, withRouter } from "react-router-dom";
+import { HashRouter, BrowserRouter, Route, withRouter, Switch, Redirect} from "react-router-dom";
 import Navbar from './components/Navbar/Navbar';
 import UsersContainer from "./components/Users/UsersContainer";
 import News from "./components/News/News";
@@ -13,10 +13,10 @@ import { initializeApp } from '../src/redux/app-reducer';
 import { compose } from "redux";
 import Preloader from "./components/common/Preloader/Preloader";
 import store from "./redux/redux-store";
-import { withSuspense } from "./hoc/withSuspense";
+// import { withSuspense } from "./hoc/withSuspense"; //95 
 
-// import DialogsContainer from "./components/Dialogs/DialogsContainer";
-// import ProfileContainer from './components/Profile/ProfileContainer';
+// import DialogsContainer from "./components/Dialogs/DialogsContainer"; //95
+// import ProfileContainer from './components/Profile/ProfileContainer'; //95
 const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
 const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
@@ -30,12 +30,13 @@ class App extends Component {
             return <Preloader />
         }
         return (
-            <HashRouter>
-                {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
+            // <HashRouter> //95
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <div className='app-wrapper'>
                     <HeaderContainer />
                     <Navbar />
                     <div className='app-wrapper-content'>
+
                         {/* 94 pots & me version*/}
                         <React.Suspense fallback={<div>Loading...</div>}>
                             <Route path='/dialogs' render={() => <DialogsContainer />} />
@@ -56,15 +57,19 @@ class App extends Component {
                             //     </React.Suspense>
                             // }
                         } /> */}
-                        <Route path='/users' render={() => <UsersContainer />} />
-                        <Route path='/news' render={() => <News />} />
-                        <Route path='/music' render={() => <Music />} />
-                        <Route path='/settings' render={() => <Settings />} />
-                        <Route path='/login' render={() => <LoginPage />} />
+                        <Switch>
+                            <Route path='/' render={() => <Redirect to={'/profile'} />} />
+                            <Route path='/users' render={() => <UsersContainer />} />
+                            <Route path='/news' render={() => <News />} />
+                            <Route path='/music' render={() => <Music />} />
+                            <Route path='/settings' render={() => <Settings />} />
+                            <Route path='/login' render={() => <LoginPage />} />
+                            <Route path='*' render={() => <div>NOT FOUND</div>} />
+                        </Switch>
                     </div>
                 </div>
-            </HashRouter>
-            // </BrowserRouter>
+                {/* </HashRouter> //95*/}
+            </BrowserRouter>
         );
     }
 }
